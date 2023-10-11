eb7bcfd0ee02043d8f5bb3b3c113a227b38dbb18
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2023-10-10 23:21:51 -0700

    Setup.sh variables removed export

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,2 @@
-export ConnectAllUrl=https://connectall255.clarityrox.com/ConnectAll:8090
-export ConnectAllApiKey=2153c01c-4d8b-4e20-87a7-53b276f1448a
+ConnectAllUrl=https://connectall255.clarityrox.com/ConnectAll:8090
+ConnectAllApiKey=2153c01c-4d8b-4e20-87a7-53b276f1448a
