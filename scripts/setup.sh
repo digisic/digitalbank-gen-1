32b3fe17228e0cd9fe2136ad5c61ed82120b3509
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2023-10-11 07:12:17 -0700

    Fixed the Post record

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
-ConnectAllUrl=http://connectall255.clarityrox.com/ConnectAll:8090
+ConnectAllUrl=http://connectall255.clarityrox.com:8090
 ConnectAllApiKey=2153c01c-4d8b-4e20-87a7-53b276f1448a
 AutomationName=BuildResults
