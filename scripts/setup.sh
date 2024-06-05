f3840deb1910ed29901413c45298ac83a2d2dfa8
Johnathan McGowan <140761666+jmcg-broadcom@users.noreply.github.com>
2024-06-05 15:35:20 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
-ConnectAllUrl=http://connectall255.clarityrox.com:8090
-ConnectAllApiKey=2153c01c-4d8b-4e20-87a7-53b276f1448a
+ConnectAllUrl=http://connectall255.clarityrox.com/ua
+ConnectAllApiKey=b656ec7b-9ccc-4347-9967-240c1b81130c
 AutomationName=BuildResults
