3654ea8dceac7840c8db9284bdb0b4c1d0d0851d
Johnathan McGowan <140761666+jmcg-broadcom@users.noreply.github.com>
2024-06-05 23:35:22 -0400

    Delete scripts/get_results.groovy

--- scripts/get_results.groovy
+++
@@ -1,8 +0,0 @@
-def buildStatus = currentBuild.result
-echoe "Build Status: $buildStatus";
-env.BUILD_RESULT = buildStatus
-echo "Environment Build Status: ${env.BUILD_RESULT}"
-
-sh '''
-echo "Build status: ${BUILD_RESULT}"
-'''
