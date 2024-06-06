0015c11dd602ee57d7b0c15d7cfc40c317b92351
Johnathan McGowan <140761666+jmcg-broadcom@users.noreply.github.com>
2024-06-05 23:17:06 -0400

    Update get_results.groovy

--- scripts/get_results.groovy
+++ scripts/get_results.groovy
@@ -1,3 +1,4 @@
 def buildStatus = currentBuild.result
+echoe "Build Status: $buildStatus";
 env.BUILD_RESULT = buildStatus
-echo "Build status: ${env.BUILD_RESULT}"
+echo "Environment Build Status: ${env.BUILD_RESULT}"
